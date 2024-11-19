684793e913c91d48d26ad02c4ac5e5ac072085d5
AKU JEDAN. KAMU SIAPA? <100419206+almightyjedan@users.noreply.github.com>
2024-11-19 18:23:04 +0700

    PRAK 8

---
+++ PRAK 8/tailwind-layout/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  darkMode: 'class', // Menambahkan opsi dark mode
+  theme: {
+    extend: {
+      colors: {
+        primary: '#a6c1ee',
+        secondary: '#fbc2eb',
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+    }
+    },
+  },
+  plugins: [],
+}
